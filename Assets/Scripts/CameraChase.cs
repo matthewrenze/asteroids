855c8208c1f5df9ae2947697a2482d9a41937f34
Matthew Renze <matthew.renze@gmail.com>
2015-07-12 11:26:23 -0500

    Added 3D chase camera mode

--- Assets/Scripts/CameraChase.cs
+++ Assets/Scripts/CameraChase.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class CameraChase : MonoBehaviour
 {
+    public bool IsChase = false;
+    
     private GameObject Player;
 
 	// Use this for initialization
@@ -17,12 +19,54 @@ public class CameraChase : MonoBehaviour
 	    if (Player == null) 
             return;
 
-	    var x = Player.transform.position.x + 15;
+	    if (Input.GetKeyDown(KeyCode.LeftAlt))
+	        IsChase = !IsChase;
 
-	    var y = gameObject.transform.position.y;
+	    if (IsChase)
+	        ShowChase();
+	    else
+	        ShowOverhead();
+    }
 
-	    var z = Player.transform.position.z;
+    private void ShowOverhead()
+    {
+        var x = Player.transform.position.x + 15f;
 
-	    gameObject.transform.position = new Vector3(x, y, z);
+        var y = Player.transform.position.y + 15f;
+
+        var z = Player.transform.position.z;
+
+        gameObject.transform.position = new Vector3(x, y, z);
+
+        var rx = -0.3f;
+
+        var ry = 0.7f;
+
+        var rz = -0.3f;
+
+        var rw = -0.7f;
+
+        gameObject.transform.rotation = new Quaternion(rx, ry, rz, rw);
+    }
+
+    private void ShowChase()
+    {
+        var x = Player.transform.position.x + 7.5f;
+
+        var y = Player.transform.position.y + 2.5f;
+
+        var z = Player.transform.position.z;
+
+        gameObject.transform.position = new Vector3(x, y, z);
+
+        var rx = 0.0f;
+
+        var ry = 0.7f;
+
+        var rz = 0.0f;
+
+        var rw = -0.7f;
+
+        gameObject.transform.rotation = new Quaternion(rx, ry, rz, rw);
     }
 }
