21703b199aec3f41836c76ac9267eb427f7ff450
Matthew Renze <matthew.renze@gmail.com>
2015-07-12 10:46:00 -0500

    Added acceleration to ship

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -4,7 +4,8 @@ using System.Collections;
 
 public class ShipMovement : MonoBehaviour
 {
-    public float MovementSpeed = 10f;
+    public float AccelerationRate = 0.1f;
+    public float MovementSpeed = 0.5f;
     public float WarpSpeed = 100f;
     public float RotationSpeed = 100f;
 
@@ -16,38 +17,97 @@ public class ShipMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
     {
-	    if (Input.GetKey(KeyCode.UpArrow) 
+	    HandleMovement();
+
+	    HandleRotation();
+        
+	    CreateImpulseBubbles();
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)
             && Input.GetKey(KeyCode.LeftControl))
-	    {
-            gameObject.transform.Translate(-WarpSpeed * Time.deltaTime, 0, 0);
+            Warp();
 
-            gameObject.transform.localScale =
-                new Vector3(gameObject.transform.localScale.x + 0.1f, 1, 1);
-	    }         
+        else if (Input.GetKey(KeyCode.UpArrow))
+            MoveForward();
 
-	    else if (Input.GetKey(KeyCode.UpArrow))
-	        gameObject.transform.Translate(-MovementSpeed * Time.deltaTime, 0, 0);
-	    
         else if (Input.GetKey(KeyCode.DownArrow))
-            gameObject.transform.Translate(MovementSpeed * Time.deltaTime, 0, 0);
+            MoveBackward();
 
+        else
+            Decelerate();
+
+        gameObject.transform.Translate(-MovementSpeed*Time.deltaTime, 0, 0);
+
+        if (!Input.GetKey(KeyCode.UpArrow)
+            || !Input.GetKey(KeyCode.LeftControl))
+            UnWarp();
+    }
+
+    private void HandleRotation()
+    {
         if (Input.GetKey(KeyCode.LeftArrow))
-            gameObject.transform.Rotate(0, -RotationSpeed * Time.deltaTime, 0);
+            RotateLeft();
 
         else if (Input.GetKey(KeyCode.RightArrow))
-            gameObject.transform.Rotate(0, RotationSpeed * Time.deltaTime, 0);
+            RotateRight();
+    }
 
-        if (!Input.GetKey(KeyCode.UpArrow) 
-            || !Input.GetKey(KeyCode.LeftControl))
-        {
-            var newXScale = gameObject.transform.localScale.x - 0.5f;
+    private void Warp()
+    {
+        gameObject.transform.Translate(-WarpSpeed*Time.deltaTime, 0, 0);
 
-            var xScale = Math.Max(newXScale, 1);
+        gameObject.transform.localScale =
+            new Vector3(gameObject.transform.localScale.x + 0.1f, 1, 1);
+    }
 
-	        if (gameObject.transform.localScale.x > 1)
-                gameObject.transform.localScale = 
-                    new Vector3(xScale, 1, 1);
-	    }
-	        
+    private void MoveForward()
+    {
+        if (MovementSpeed < 10)
+            MovementSpeed += AccelerationRate;        
+    }
+
+    private void MoveBackward()
+    {
+        if (MovementSpeed > -10)
+            MovementSpeed -= AccelerationRate;
+    }
+
+    private void Decelerate()
+    {
+        if (MovementSpeed > 0)
+            MovementSpeed -= AccelerationRate;
+        else
+            MovementSpeed += AccelerationRate;
+    }
+
+    private void RotateLeft()
+    {
+        gameObject.transform.Rotate(0, -RotationSpeed * Time.deltaTime, 0);
+    }
+
+    private void RotateRight()
+    {
+        gameObject.transform.Rotate(0, RotationSpeed * Time.deltaTime, 0);
+    }
+
+    private void UnWarp()
+    {
+        var newXScale = gameObject.transform.localScale.x - 0.5f;
+
+        var xScale = Math.Max(newXScale, 1);
+
+        if (gameObject.transform.localScale.x > 1)
+            gameObject.transform.localScale =
+                new Vector3(xScale, 1, 1);
+    }
+
+    private void CreateImpulseBubbles()
+    {
+        var warpBubbles = gameObject.GetComponentInChildren<ParticleSystem>();
+
+        warpBubbles.emissionRate = Math.Abs(MovementSpeed);
     }
 }
