bae7da0a140df6f47e40cb556c6536236476652c
Matthew Renze <matthew.renze@gmail.com>
2015-08-01 10:06:45 -0500

    Added additional camera perspectives

--- Assets/Scripts/CameraChase.cs
+++ Assets/Scripts/CameraChase.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts;
+using UnityEngine;
 using System.Collections;
 
 public class CameraChase : MonoBehaviour
 {
-    public bool IsChase = false;
+    public CameraMode CameraMode = CameraMode.Overhead;
     
     private GameObject Player;
 
@@ -19,16 +20,47 @@ public class CameraChase : MonoBehaviour
 	    if (Player == null) 
             return;
 
-	    if (Input.GetKeyDown(KeyCode.LeftAlt))
-	        IsChase = !IsChase;
+	    if (Input.GetKeyDown(KeyCode.LeftShift))
+            CameraMode = (CameraMode)(((int)CameraMode + 1) % -4);
 
-	    if (IsChase)
-	        ShowChase();
-	    else
+        if (Input.GetKeyDown(KeyCode.RightShift))
+            CameraMode = (CameraMode)(((int)CameraMode + 1) % 4);
+
+	    if (CameraMode == CameraMode.Overhead)
 	        ShowOverhead();
+
+        else if (CameraMode == CameraMode.Orthogonal)
+	        ShowOrthogonal();
+
+        else if (CameraMode == CameraMode.Chase)
+            ShowChase();
+
+        else if (CameraMode == CameraMode.FirstPerson)
+            ShowFirstPerson();
     }
 
     private void ShowOverhead()
+    {
+        var x = Player.transform.position.x;
+
+        var y = Player.transform.position.y;
+
+        var z = Player.transform.position.z + 50f;
+
+        gameObject.transform.position = new Vector3(x, y, z);
+
+        var rx = 0.7f;
+
+        var ry = 0.7f;
+
+        var rz = 0.0f;
+
+        var rw = 0.0f;
+
+        gameObject.transform.rotation = new Quaternion(rx, ry, rz, rw);
+    }
+
+    private void ShowOrthogonal()
     {
         var x = Player.transform.position.x - 15f;
 
@@ -68,5 +100,29 @@ public class CameraChase : MonoBehaviour
         var rw = 0.5f;
 
         gameObject.transform.rotation = new Quaternion(rx, ry, rz, rw);
+    }
+
+    private void ShowFirstPerson()
+    {
+        var x = Player.transform.position.x;
+
+        var y = Player.transform.position.y;
+
+        var z = Player.transform.position.z + 1;
+
+        gameObject.transform.position = new Vector3(x, y, z);
+
+        var rx = 0.5f;
+
+        var ry = 0.5f;
+
+        var rz = 0.5f;
+
+        var rw = 0.5f;
+
+        // START HERE: Fix first-person rotation
+        gameObject.transform.rotation = Player.transform.rotation;
+
+        gameObject.transform.Rotate(0, 90, 90);
     }
 }
---
+++ Assets/Scripts/CameraMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    public enum CameraMode
+    {
+        Overhead = 0,
+        Orthogonal = 1,
+        Chase = 2,
+        FirstPerson = 3
+    }
+}
